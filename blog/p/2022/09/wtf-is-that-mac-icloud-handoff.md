87cbf41089f269931c39c8ee7506107f91aebbe3
Fritz Lin <uxfritz@163.com>
2022-10-01 21:12:22 +0800

    blog update

---
+++ blog/p/2022/09/wtf-is-that-mac-icloud-handoff.md
@@ -0,0 +1,64 @@
+# 😡 什么神仙功能！🍎iCloud 的“接力”功能：“在设备之间传送网页”？
+
+> 妈妈再也不用担心，我正在浏览的🔞网页被传送到iPad上了
+
+*#苹果 #Apple #iCloud #iPad #Mac #接力 #Handoff #信息安全 #个人隐私*
+
+*2022-09-28*
+
+简体中文 | [English](./wtf-is-that-mac-icloud-handoff-en.md)
+
+<img width=600 src=WX20220928-180550.png>
+
+我自己平时很少使用iPad，但当我发现这个问题时我震惊了 😱
+
+设想一下：
+
+> 你在Mac上正在浏览的是 🔞 网页，在iPad屏幕的右下角会有提示，并且可以直接打开
+
+> 你的家人碰巧在使用你的iPad（登录着相同的AppleID）
+
+> iPad右下角会提示，并且能够一键打开你正在浏览的网页 😲
+
+> 甚至是你日常使用的Chrome！Safari以外的浏览器，依然无法幸免
+
+这就来到了文章的标题：😡 **这是什么神仙功能！What the Hell is that?!**
+
+### 个人隐私安全
+
+虽说早有耳闻苹果iCloud同步本身就不安全（如相片同步）
+
+所以iCloud同步方面的个人隐私风险我早有控制（通过合理配置，结合个人日常使用方式）
+
+比如我是关闭了相片同步，日常使用的是Android手机+Mac电脑，基本没有问题
+
+### 如何解决
+
+尝试过关闭iCloud同步中的“Safari数据”，无法解决
+
+当我搜索"ipad safari can view mac"，终于得到了想要的结果
+
+> **在 Mac 上的 Safari 浏览器中查看在其他设备上打开的网页 - 
+《Safari 浏览器使用手册》**<br>
+> https://support.apple.com/zh-cn/guide/safari/sfri40726/mac
+
+> **在 Mac 上使用“接力”回到上次离开的地方 - 
+《macOS 使用手册》**<br>
+> https://support.apple.com/zh-cn/guide/mac-help/mchl732d3c0a/mac
+
+以及“接力”这个功能的[正式介绍](https://support.apple.com/zh-cn/HT209455)：
+
+> **使用接力在其他设备上继续完成任务**<br>
+> 借助接力，您可以在一台设备上开始工作，再切换到附近的另一台设备上继续工作。
+
+阅读完相关文档后，终于弄清楚了这是个什么鬼👻，以及如何解决
+
+### 打开或关闭“接力”
+
+> - 在您的 Mac 上：选取苹果菜单 >“系统偏好设置”，点按“通用” ，然后选择“允许在这台 Mac 和 iCloud 设备之间使用接力”（位于面板底部）。若要将其关闭，请取消选择该选项。
+> - 在 iPad、iPhone 或 iPod touch 上：前往“设置”>“通用”>“隔空播放与接力”，然后打开或关闭“接力”。
+> - 在 Apple Watch 上：在 iPhone 上的 Apple Watch App 中，前往“我的手表”>“通用”，然后打开或关闭“启用接力”。
+
+在Mac点击左上角，打开“系统设置”>“通用“，关闭这个什么狗屁 “接力”（“Handoff”）💩
+
+![](WX20220928-175618.png)
--- blog/vendor/blog.js
+++ blog/vendor/blog.js
@@ -190,7 +190,7 @@
   // https://stackoverflow.com/questions/18862256/how-to-detect-emoji-using-javascript
   // +modification title-favicon bugfix: recognize emoji `✋🏻`
   var emojiPrefixRegex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+/
-  var anyPrefixRegex = /^(?:(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+|.)/
+  var anyPrefixRegex = new RegExp(emojiPrefixRegex.toString().replace(/^\/\^(.+)\/$/, '^(?:$1|.)'))
 
   // Use emoji as favicon in websites
   // https://stackoverflow.com/questions/59431371/use-emoji-as-favicon-in-websites
@@ -206,17 +206,22 @@
       ctx.fillStyle = isDarkMode ? 'orange' : '#336699' // for text color
       ctx.textAlign = 'center'
       ctx.fillText(s, 16, 24)
-      var link = document.querySelector('link[rel=icon]')
-      var hasLinkAlready = !!link
-      if (!hasLinkAlready) {
-        link = document.createElement('link')
-        link.setAttribute('rel', 'icon')
-      }
-      link.setAttribute('href', canvas.toDataURL())
-      if (!hasLinkAlready) {
-        var parent = document.querySelector('head') || document.documentElement
-        parent.appendChild(link)
-      }
+      var dataUrl = canvas.toDataURL()
+
+      var parent = document.querySelector('head') || document.documentElement
+      var rels = ['icon']
+      rels.forEach(key => {
+        var link = document.querySelector('link[rel=' + key + ']')
+        if (link) {
+          link.setAttribute('href', dataUrl)
+        } else {
+          link = document.createElement('link')
+          link.setAttribute('rel', key)
+          link.setAttribute('href', dataUrl)
+          parent.appendChild(link)
+        }
+      })
+
       lastFavicon = s
       return true // success flag
     } catch (err) {
@@ -229,9 +234,22 @@
     if (lastFavicon) setFavicon(lastFavicon)
   })
 
+  function detectShouldApplyFavicon() {
+    var ua = navigator.userAgent
+    var isMobile = /Mobile[\/ ]|Android|iPad/.test(ua) // confidence: high
+    var isHuaweiBr = /HuaweiBrowser/.test(ua) // confidence: high
+    var isWechat = /MicroMessenger|Wechat|Weixin/.test(ua) // confidence: high
+    var isQQ = /M?QQBrowser/.test(ua) // confidence: high
+    var isSafari = /Version\/[\d.]+\s+Safari/.test(ua) // confidence: low
+    return !isWechat && !isSafari && (isMobile ? isHuaweiBr || isQQ : true)
+  }
+  var shouldApplyFavicon = detectShouldApplyFavicon()
+
   function autoTitleFavicon(mainTitle, emojiOnly) {
     var regex = emojiOnly ? emojiPrefixRegex : anyPrefixRegex
     var navTitle = mainTitle
+    if (!shouldApplyFavicon) return navTitle
+
     var matched = mainTitle.match(regex)
     if (matched) {
       var prefix = matched[0]
